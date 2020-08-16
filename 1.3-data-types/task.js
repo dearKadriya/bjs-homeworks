78f6982289cb3fe91db480e737f08ed60cc2b7ac
Artem <a.kosnikov@growfood.pro>
2020-08-16 23:37:53 +0300

    tasks solved

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,30 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (isNaN(percent) == true || percent == 0) {
+        return (`Параметр процент содержит неправильное значение ${window.percent.value}`);
+    }else if (isNaN(contribution)) {
+        return (`Параметр первоначальный взнос содержит неправильное значение ${window.contribution.value}`);
+    } else if (isNaN(amount)) {
+        return(`Параметр сумма кредита содержит неправильное значение ${window.amount.value}`);
+    } else {
+        let creditAmount = amount - contribution;
+        let creditTime = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear()));
+        let percentMonthly = (percent / 100) / 12;
+        let payment = creditAmount * (percentMonthly + (percentMonthly / (((1 + percentMonthly) ** creditTime) - 1)));
+        let totalAmount = (payment * creditTime).toFixed(2);
+        console.log(totalAmount);
+        return Number(totalAmount);
+    }
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+
+    if (name == ' ' || name === null || name === undefined || name.length == 0) {
+        greeting = `Привет, мир! Меня зовут Аноним`;
+    }
+    return greeting;
+
+
 }
